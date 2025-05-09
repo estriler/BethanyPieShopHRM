15a08851d127418e95fa41e212c6caa68a82994a
Eric S <estriler@hotmail.com>
2025-05-09 13:57:05 -0500

    learning about DateTime

--- BethanyPieShopHRM/Program.cs
+++ BethanyPieShopHRM/Program.cs
@@ -40,4 +40,19 @@ if (currentWeekWage > 1000)
 
 numberOfEmployees--;
 
+DateTime hireDate = new DateTime(2025, 03, 28, 14, 30, 00);
+//Constructor syntax in year, month, day, hour, minute, second
 
+DateTime exitDate = new DateTime(2075, 03, 28);
+
+DateTime startDate = hireDate.AddDays(15);
+Console.WriteLine(startDate);
+
+DateTime startHour = DateTime.Now;
+TimeSpan workTime = new TimeSpan(8, 0, 0);
+DateTime endHour = startHour.Add(workTime);
+
+Console.WriteLine(startHour.ToLongDateString());
+Console.WriteLine(endHour.ToShortDateString());
+
+Console.ReadLine();
