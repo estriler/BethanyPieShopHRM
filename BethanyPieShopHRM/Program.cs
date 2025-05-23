7d9dc5300facc28ef8d7676d661e7851ab21afcf
Eric S <estriler@hotmail.com>
2025-05-22 19:01:34 -0500

    learning about if statements

--- BethanyPieShopHRM/Program.cs
+++ BethanyPieShopHRM/Program.cs
@@ -72,4 +72,7 @@ Console.WriteLine("Age is 23: " + a);
 bool b = age >= 23;
 Console.WriteLine("Age is greater than or equal to 23: " + b);
 
+bool c = (age >= 18) && (age <= 65);
+Console.WriteLine("Age is between 18 and 65: " + c);
+
 
