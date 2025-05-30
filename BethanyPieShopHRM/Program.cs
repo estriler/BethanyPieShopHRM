62c9b1e6b7688b4deae00d599f3a5aef43ebad6b
Eric S <estriler@hotmail.com>
2025-05-29 20:08:05 -0500

    learning about method overloading

--- BethanyPieShopHRM/Program.cs
+++ BethanyPieShopHRM/Program.cs
@@ -155,8 +155,9 @@ using BethanyPieShopHRM;
 
 int amount = 1234;
 int months = 12;
+int bonus = 1000;
 
-int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
+int yearlyWage = Utilities.CalculateYearlyWage(amount, months, bonus);
 
 Console.WriteLine($"Yearly wage: {yearlyWage}");
 
